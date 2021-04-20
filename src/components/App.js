55f704b355498d35b3d8708065dd2b1e6931c8b5
ZerZoul <a.adnot@gmail.com>
2021-04-20 11:56:24 +0200

    create slidebar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import NavBar from './NavBar'
+import SlideBar from './SlideBar'
 function App() {
   return (
     <div>      
         <NavBar />
+        <SlideBar />
     </div>
   );
 }
---
+++ src/components/SlideBar.js
@@ -0,0 +1,9 @@
+const SlideBar = () => {
+    return (
+        <div className="slide-bar">
+            <h2>SlideBar</h2>
+        </div>
+    );
+}
+ 
+export default SlideBar;
