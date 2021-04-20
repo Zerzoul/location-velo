996b327fb83a5dd478aabcf3d30532debf0077c9
ZerZoul <a.adnot@gmail.com>
2021-04-20 10:40:40 +0200

    Reorganize folders

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello from react
+      </header>
+    </div>
+  );
+}
+
+export default App;
