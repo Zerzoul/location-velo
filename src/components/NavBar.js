80c9ee1b44d616c5268a99e0a00f13443f26b669
ZerZoul <a.adnot@gmail.com>
2021-04-22 10:19:03 +0200

    change the design of the side bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,11 @@ const NavBar = () => {
     return (
         <header>
             <div className="nav-header">
-                <div className="nav-header-logo">
-                    <img src={logo} alt="location de vélo Lyonnais"/>
-                </div>
                 <div className="nav-header-link">
-                    <a href="/">Home</a>
+                    <a href="/">home</a>
+                    <div className="nav-header-logo">
+                        <img src={logo} alt="location de vélo Lyonnais"/>
+                    </div>
                     <a href="/connexion">connexion</a>
                 </div>
             </div>
