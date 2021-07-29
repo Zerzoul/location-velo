5c13496c36e2fe2298037f52f56b25ec8b3ce449
Aurelien Adnot <aurelien.adnot@web-atrio.com>
2021-07-29 11:29:05 +0200

    creation of a carouselItems obj

---
+++ src/data/carsouelObj.js
@@ -0,0 +1,23 @@
+import bicycle1 from "../assests/bicycle-image-1.jpg";
+import bicycle2 from "../assests/bicycle-image-2.jpg";
+import bicycle3 from "../assests/bicycle-image-3.jpg";
+import bicycle4 from "../assests/bicycle-image-4.jpg";
+
+export const carouselItems = [
+    {
+        description: 'description',
+        img: 'bicycle1'
+    },
+    {
+        description: 'description',
+        img: 'bicycle2'
+    },
+    {
+        description: 'description',
+        img: 'bicycle3'
+    },
+    {
+        description: 'description',
+        img: 'bicycle4'
+    },
+]
